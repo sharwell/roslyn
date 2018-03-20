fe079ad4413dfc2ca0608dde0ff08ef61976ca45
Sam Harwell <sam.harwell@microsoft.com>
2018-03-20 14:10:35 -0500

    Add Opt and Opt<T> to Workspaces

---
+++ src/Workspaces/Core/Portable/Opt.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis
+{
+    internal static class Opt
+    {
+        /// <summary>
+        /// An optional reference which does not have a value. This is equivalent to the <see langword="null"/> literal
+        /// when working with <see cref="Opt{T}"/>.
+        /// </summary>
+        public static readonly MissingValue Missing;
+
+        public static Opt<T> From<T>(T value)
+            where T : class
+        {
+            return Opt<T>.From(value);
+        }
+
+        public static Opt<T> FromNullable<T>(T value)
+            where T : class
+        {
+            return Opt<T>.FromNullable(value);
+        }
+
+        public readonly struct MissingValue
+        {
+        }
+    }
+}
---
+++ src/Workspaces/Core/Portable/Opt`1.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis
+{
+    /// <summary>
+    /// Represents a reference which is allowed to have the value <see langword="null"/>. It is the equivalent of
+    /// <see cref="Nullable{T}"/> for reference types: an instance either "does not have a value", or "has a non-null
+    /// value".
+    /// </summary>
+    /// <remarks>
+    /// <para>This structure may be used for passing optional arguments.</para>
+    /// </remarks>
+    /// <typeparam name="T">The type.</typeparam>
+    internal readonly struct Opt<T>
+        where T : class
+    {
+        public static readonly Opt<T> Missing;
+
+        private readonly T _value;
+
+        private Opt(T value)
+        {
+            _value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public bool HasValue => _value != null;
+        public T Value => _value ?? throw new InvalidOperationException();
+        public T ValueOrDefault => _value;
+
+        public static implicit operator Opt<T>(Opt.MissingValue missing) => Missing;
+
+        public static implicit operator Opt<T>(T value) => FromNullable(value);
+        public static explicit operator T(Opt<T> value) => value.Value;
+
+        public static bool operator ==(Opt<T> x, Opt<T> y)
+            => x._value == y._value;
+
+        public static bool operator !=(Opt<T> x, Opt<T> y)
+            => x._value != y._value;
+
+        public static Opt<T> From(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return new Opt<T>(value);
+        }
+
+        public static Opt<T> FromNullable(T value)
+            => new Opt<T>(value);
+
+        public Opt<TOther> As<TOther>()
+            where TOther : class
+        {
+            return new Opt<TOther>(_value as TOther);
+        }
+
+        public override int GetHashCode()
+            => EqualityComparer<T>.Default.GetHashCode(_value);
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Opt<T> other)
+            {
+                return Equals(other);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Opt<T> obj)
+            => EqualityComparer<T>.Default.Equals(_value, obj._value);
+    }
+}
